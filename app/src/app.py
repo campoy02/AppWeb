9f0a6221da9e8fd83bb859af0540176880bb0d13
campoy02 <a21490531@itmexicali.edu.mx>
2025-04-08 13:55:44 -0700

    nose

--- app/src/app.py
+++ app/src/app.py
@@ -4,45 +4,56 @@ app = Flask(__name__)
 
 ####### Solo poner las rutas que llevan a las paginas principales aqui #######
 
+
 @app.route("/")
 def index():
     return render_template("public/index.html")
+
 
 @app.route("/inicio")
 def inicio():
     return render_template("public/index.html")
 
+
 @app.route("/camiseta")
 def camiseta():
     return render_template("public/Camiseta.html")
+
 
 @app.route("/favoritos")
 def favoritos():
     return render_template("public/Favoritos.html")
 
+
 @app.route("/formulario")
 def formulario():
     return render_template("public/Formulario.html")
+
 
 @app.route("/historial")
 def historial():
     return render_template("public/Historial.html")
 
+
 @app.route("/perfil")
 def perfil():
     return render_template("public/perfil.html")
+
 
 @app.route("/recetas")
 def recetas():
     return render_template("public/Recetas.html")
 
+
 @app.route("/resultados")
 def resultados():
     return render_template("public/Resultados.html")
 
+
 @app.route("/subir")
 def subir():
     return render_template("public/Subir.html")
+
 
 @app.route("/tienda")
 def tienda():
@@ -50,14 +61,12 @@ def tienda():
 
 ##############################################################################
 
-##               El resto del desmadre ponerlo aqui                         ## 
-
+##               El resto del desmadre ponerlo aqui                         ##
 
 
 ##############################################################################
 
 
-
-#No poner nada debajo de main#
+# No poner nada debajo de main#
 if __name__ == "__main__":
     app.run(debug=True)
