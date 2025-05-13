504a9edb6551de4bc51aeedf5f37fe1a959ea43c
Ary <a20491106@itmexicali.edu.mx>
2025-05-13 10:44:49 -0700

    Arreglos  / Cambios a la BDD

--- app/src/app.py
+++ app/src/app.py
@@ -27,11 +27,6 @@ login_manager_app = LoginManager(app)
 app.config['UPLOAD_PATH'] = 'app/src/static/uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
 
-####### Maneja los archivos aceptados para las imagenes                #######
-
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 ####### Solo poner las rutas que llevan a las paginas principales aqui #######
 
@@ -79,7 +74,6 @@ def recetas():
 @app.route("/recetas/<int:id>")
 def recetasid(id):
     PaginaRec = ModelPagina.RecetaPorID(db, id)
-    print(PaginaRec)
     return render_template("public/RecetasID.html", receta=PaginaRec)
 
 
@@ -131,7 +125,7 @@ def subir():
             if not os.path.exists(app.config['UPLOAD_PATH']):
                 os.makedirs(app.config['UPLOAD_PATH'])
             # Favor de actualizar los comentarios en caso de cambiar alguno de estos limites
-            TMAX = 5 * 1024 * 1024  # Tamaño maximo actual: 7MB por imagen
+            TMAX = 5 * 1024 * 1024  # Tamaño maximo actual: 5MB por imagen
             RMAX = (1500, 1200)  # Resolucion maxima actual:  1000x1200
 
             if img.content_length > TMAX or img2.content_length > TMAX:
